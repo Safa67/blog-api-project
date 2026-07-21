2051928698d7849a2f136f6e479039b55a39518a
Safa67 <safa.b6700@gmail.com>
2026-07-21 10:29:11 +0300

    swagger eklendi

---
+++ src/main/java/com/safa/blog_api_project/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.safa.blog_api_project.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                .title("Blog API Projesi")
+                        .version("1.0.0")
+                        .description("Bu API; blog yazıları, kategoriler, etiketler ve yorumlar için geliştirilmiş temel CRUD operasyonlarını içerir.")
+        );
+    }
+}
